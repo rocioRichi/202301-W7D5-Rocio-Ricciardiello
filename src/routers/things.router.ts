28f2b37f49ba226fe3cf4b57970e6cdf6ac9d546
rocioRichi <paragit@gmail.com>
2023-03-05 14:00:27 +0100

    Add non use files

--- src/routers/things.router.ts
+++ src/routers/things.router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { ThingsController } from '../controllers/things.controller.js';
 import { ThingsMongoRepo } from '../repository/things.mongo.repo.js';
 import { logged } from '../interceptors/logged.js';
-import { authorized } from '../interceptors/authorized.js';
+// import { authorized } from '../interceptors/authorized.js';
 import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
 import createDebug from 'debug';
 // Alt import { Interceptor } from '../interceptors/interceptor.js';
@@ -20,15 +20,15 @@ const controller = new ThingsController(repoThings, repoUsers);
 thingsRouter.get('/', controller.getAll.bind(controller));
 thingsRouter.get('/:id', controller.get.bind(controller));
 thingsRouter.post('/', logged, controller.post.bind(controller));
-thingsRouter.patch(
-  '/:id',
-  logged,
-  (req, resp, next) => authorized(req, resp, next, repoThings),
-  controller.patch.bind(controller)
-);
-thingsRouter.delete(
-  '/:id',
-  logged,
-  (req, resp, next) => authorized(req, resp, next, repoThings),
-  controller.delete.bind(controller)
-);
+// thingsRouter.patch(
+//   '/:id',
+//   logged,
+//   (req, resp, next) => authorized(req, resp, next, repoThings),
+//   controller.patch.bind(controller)
+// );
+// thingsRouter.delete(
+//   '/:id',
+//   logged,
+//   (req, resp, next) => authorized(req, resp, next, repoThings),
+//   controller.delete.bind(controller)
+// );
